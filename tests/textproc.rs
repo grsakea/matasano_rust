5b67ab5fc7a027efef7e5cc77e52044f50a93dfa
grsakea <grsakea@users.noreply.github.com>
2018-08-04 20:17:33 +0200

    Fix tests

--- tests/textproc.rs
+++ tests/textproc.rs
@@ -1,9 +1,9 @@
-extern crate hello_world;
+extern crate matasano;
 
 #[test]
 fn test_hamming() {
     let text1 = "this is a test".as_bytes().to_vec();
     let text2 = "wokka wokka!!!".as_bytes().to_vec();
-    assert_eq!(hello_world::textproc::hamming_distance(&text1, &text2), 37);
+    assert_eq!(matasano::textproc::hamming_distance(&text1, &text2), 37);
 }
 
